af50045722478a472429fe87f2b43e8d616c7951
talalaevk <kirill.mipt2013@gmail.ccom>
2024-03-04 15:39:45 +0000

    logreg dvc

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import yaml
 import os
 import joblib
@@ -18,23 +17,18 @@ def fit_model():
 	# реализуйте основную логику шага с использованием гиперпараметров
 
     cat_features = data.select_dtypes(include='object')
-    potential_binary_features = cat_features.nunique() == 2
-
-    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
-    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
     num_features = data.select_dtypes(['float'])
 
     preprocessor = ColumnTransformer(
         [
-            ('binary', OneHotEncoder(drop=params['one_hot_drop']), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(drop=params['one_hot_drop']), cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = CatBoostClassifier(auto_class_weights=params['auto_class_weights'])
+    model = LogisticRegression(C=params['C'], penalty=params['penalty'])
 
     pipeline = Pipeline(
         [
